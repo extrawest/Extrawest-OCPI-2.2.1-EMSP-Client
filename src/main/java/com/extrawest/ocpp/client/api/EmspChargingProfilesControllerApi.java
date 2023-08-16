a4efad6364828a90fc5bc4a9fde6824541977430
galyna.klymenko <galyna.klymenko@extrawest.com>
2023-08-16 12:26:02 +0300

    IP0001-263 Client generated based on yaml file through openapi-cli

---
+++ src/main/java/com/extrawest/ocpp/client/api/EmspChargingProfilesControllerApi.java
@@ -0,0 +1,149 @@
+package com.extrawest.ocpp.client.api;
+
+import com.extrawest.ocpp.client.invoker.ApiClient;
+import com.extrawest.ocpp.client.model.AbstractProfileResult;
+import com.extrawest.ocpp.client.model.ActiveChargingProfileRequestDTO;
+import com.extrawest.ocpp.client.model.ResponseFormat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-08-15T22:04:36.538444+03:00[Europe/Kiev]")
+@Component("com.extrawest.ocpp.client.api.EmspChargingProfilesControllerApi")
+public class EmspChargingProfilesControllerApi {
+    private ApiClient apiClient;
+
+    public EmspChargingProfilesControllerApi() {
+        this(new ApiClient());
+    }
+
+    @Autowired
+    public EmspChargingProfilesControllerApi(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public ApiClient getApiClient() {
+        return apiClient;
+    }
+
+    public void setApiClient(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    /**
+     * 
+     * 
+     * <p><b>200</b> - OK
+     * @param abstractProfileResult  (required)
+     * @return ResponseFormat
+     * @throws RestClientException if an error occurs while attempting to invoke the API
+     */
+    public ResponseFormat postChargingProfile(AbstractProfileResult abstractProfileResult) throws RestClientException {
+        return postChargingProfileWithHttpInfo(abstractProfileResult).getBody();
+    }
+
+    /**
+     * 
+     * 
+     * <p><b>200</b> - OK
+     * @param abstractProfileResult  (required)
+     * @return ResponseEntity&lt;ResponseFormat&gt;
+     * @throws RestClientException if an error occurs while attempting to invoke the API
+     */
+    public ResponseEntity<ResponseFormat> postChargingProfileWithHttpInfo(AbstractProfileResult abstractProfileResult) throws RestClientException {
+        Object localVarPostBody = abstractProfileResult;
+        
+        // verify the required parameter 'abstractProfileResult' is set
+        if (abstractProfileResult == null) {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'abstractProfileResult' when calling postChargingProfile");
+        }
+        
+
+        final MultiValueMap<String, String> localVarQueryParams = new LinkedMultiValueMap<String, String>();
+        final HttpHeaders localVarHeaderParams = new HttpHeaders();
+        final MultiValueMap<String, String> localVarCookieParams = new LinkedMultiValueMap<String, String>();
+        final MultiValueMap<String, Object> localVarFormParams = new LinkedMultiValueMap<String, Object>();
+
+        final String[] localVarAccepts = { 
+            "*/*"
+         };
+        final List<MediaType> localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+        final String[] localVarContentTypes = { 
+            "application/json"
+         };
+        final MediaType localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+        String[] localVarAuthNames = new String[] {  };
+
+        ParameterizedTypeReference<ResponseFormat> localReturnType = new ParameterizedTypeReference<ResponseFormat>() {};
+        return apiClient.invokeAPI("/emsp/api/2.2.1/chargingProfiles", HttpMethod.POST, Collections.<String, Object>emptyMap(), localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localReturnType);
+    }
+    /**
+     * 
+     * 
+     * <p><b>200</b> - OK
+     * @param sessionId  (required)
+     * @param activeChargingProfileRequestDTO  (required)
+     * @return ResponseFormat
+     * @throws RestClientException if an error occurs while attempting to invoke the API
+     */
+    public ResponseFormat putChargingProfile(String sessionId, ActiveChargingProfileRequestDTO activeChargingProfileRequestDTO) throws RestClientException {
+        return putChargingProfileWithHttpInfo(sessionId, activeChargingProfileRequestDTO).getBody();
+    }
+
+    /**
+     * 
+     * 
+     * <p><b>200</b> - OK
+     * @param sessionId  (required)
+     * @param activeChargingProfileRequestDTO  (required)
+     * @return ResponseEntity&lt;ResponseFormat&gt;
+     * @throws RestClientException if an error occurs while attempting to invoke the API
+     */
+    public ResponseEntity<ResponseFormat> putChargingProfileWithHttpInfo(String sessionId, ActiveChargingProfileRequestDTO activeChargingProfileRequestDTO) throws RestClientException {
+        Object localVarPostBody = activeChargingProfileRequestDTO;
+        
+        // verify the required parameter 'sessionId' is set
+        if (sessionId == null) {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'sessionId' when calling putChargingProfile");
+        }
+        
+        // verify the required parameter 'activeChargingProfileRequestDTO' is set
+        if (activeChargingProfileRequestDTO == null) {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'activeChargingProfileRequestDTO' when calling putChargingProfile");
+        }
+        
+        // create path and map variables
+        final Map<String, Object> uriVariables = new HashMap<String, Object>();
+        uriVariables.put("session_id", sessionId);
+
+        final MultiValueMap<String, String> localVarQueryParams = new LinkedMultiValueMap<String, String>();
+        final HttpHeaders localVarHeaderParams = new HttpHeaders();
+        final MultiValueMap<String, String> localVarCookieParams = new LinkedMultiValueMap<String, String>();
+        final MultiValueMap<String, Object> localVarFormParams = new LinkedMultiValueMap<String, Object>();
+
+        final String[] localVarAccepts = { 
+            "*/*"
+         };
+        final List<MediaType> localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+        final String[] localVarContentTypes = { 
+            "application/json"
+         };
+        final MediaType localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+        String[] localVarAuthNames = new String[] {  };
+
+        ParameterizedTypeReference<ResponseFormat> localReturnType = new ParameterizedTypeReference<ResponseFormat>() {};
+        return apiClient.invokeAPI("/emsp/api/2.2.1/chargingProfiles/{session_id}", HttpMethod.PUT, uriVariables, localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localReturnType);
+    }
+}
